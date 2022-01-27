89aa3a597358a53e1e5c16470494fe1a817eaeb4
leonace924 <meobius123@gmail.com>
2022-01-27 02:32:41 -0500

    refactor: clean structure and update tsconfig

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center"></main>
+
+      <footer className="flex items-center justify-center w-full h-24 border-t"></footer>
+    </div>
+  )
+}
