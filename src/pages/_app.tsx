affce2579a64db5cc8b3a7e88bf09b10d53c164e
leonace924 <meobius123@gmail.com>
2022-01-28 14:18:11 -0500

    feat: add apllo client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
+import client from '@lib/apollo-client'
+import { ApolloProvider } from '@apollo/client'
+
 import '@styles/tailwind.css'
 import '@styles/app.css'
 
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
